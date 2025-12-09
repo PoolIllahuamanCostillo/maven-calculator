e9ec565ed8f5323206e2649b4dec0273aa44394f
PoolIllahuamanCostillo <pool10illa@gmail.com>
2025-12-09 12:54:57 +0100

    Añadir operación mínimo

--- src/main/java/com/dam2/calculator/OperationRouter.java
+++ src/main/java/com/dam2/calculator/OperationRouter.java
@@ -1,5 +1,6 @@
 package com.dam2.calculator;
 
+import com.dam2.calculator.ops.OpMinimo;
 import com.dam2.calculator.ops.OpResta;
 import com.dam2.calculator.ops.OpSuma;
 
@@ -17,7 +18,7 @@ public class OperationRouter {
         switch (op.trim().toLowerCase()) {
             case "suma": return OpSuma.run(input);
             case "resta": return OpResta.run(input);
-
+            case "minimo": return OpMinimo.run(input);
             default: return "Operación no reconocida. Escribe 'help'.";
             
            
