37671fa91148e3295e501cdaa60ec542d0a03020
PoolIllahuamanCostillo <pool10illa@gmail.com>
2025-12-16 12:31:45 +0100

    cambios

--- src/main/java/com/dam2/calculator/OperationRouter.java
+++ src/main/java/com/dam2/calculator/OperationRouter.java
@@ -1,21 +1,15 @@
 package com.dam2.calculator;
 
-<<<<<<< HEAD
 import com.dam2.calculator.ops.OpMinimo;
-=======
 import com.dam2.calculator.ops.OpMax;
->>>>>>> main
 import com.dam2.calculator.ops.OpResta;
 import com.dam2.calculator.ops.OpSuma;
 import com.dam2.calculator.ops.OpDivision;
 public class OperationRouter {
 
-<<<<<<< HEAD
-    private static final String OPS_LINE = "Operaciones: suma, resta, divicion";
-=======
-    private static final String OPS_LINE = "Operaciones: suma, resta, Maximo";
->>>>>>> main
 
+    private static final String OPS_LINE = "Operaciones: suma, resta, divicion";
+    
     public static String help() {
         return OPS_LINE + "\n" +
                "Formato: operacion + numeros separados por espacios\n" +
@@ -26,19 +20,10 @@ public class OperationRouter {
         switch (op.trim().toLowerCase()) {
             case "suma": return OpSuma.run(input);
             case "resta": return OpResta.run(input);
-<<<<<<< HEAD
-<<<<<<< HEAD
             case "minimo": return OpMinimo.run(input);
-=======
             case "division": return OpDivision.run(input);
-
->>>>>>> main
-=======
             case "maximo": return OpMax.run(input);
->>>>>>> main
             default: return "Operación no reconocida. Escribe 'help'.";
-            
-           
-        }
+     }
     }
 }
